a21e88b803636b4ac550ef4ac7c14cb8c5bf9b56
JulianaAmoasei <late.parrot@gmail.com>
2023-12-29 15:08:51 -0300

    video: o que são objetos

---
+++ objeto.js
@@ -0,0 +1,10 @@
+const listaCPFs = ['12312312312', '34534534534', '98798798798'];
+
+const infosPessoas = ['nome', 'José', 'idade', 32, 'cpf', '34534534534'];
+console.log(infosPessoas[1]);
+
+const objPessoa = {
+  nome: 'José', 
+  idade: 32,
+  cpf: '34534534534'
+}
