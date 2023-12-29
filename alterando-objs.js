37eab2cfd7f5c886796214589ad829aa683fa014
JulianaAmoasei <late.parrot@gmail.com>
2023-12-29 17:10:48 -0300

    video: adicionando e alterando

---
+++ alterando-objs.js
@@ -0,0 +1,15 @@
+const cliente = {
+  nome: 'Ana',
+  idade: 32,
+  cpf: '23423423423',
+  email: 'ana@email.com'
+};
+
+console.log(cliente.nome);
+console.log(cliente.telefone);
+
+cliente.telefone = '551198769876';
+console.log(cliente.telefone);
+
+cliente.nome = 'Ana Silva';
+console.log(cliente.nome);
