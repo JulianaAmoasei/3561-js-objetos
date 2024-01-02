70166b38d286cfb4866c791d075400e8317007f6
JulianaAmoasei <late.parrot@gmail.com>
2024-01-02 19:10:49 -0300

    video: operações com JSON

--- leitura-json.js
+++ leitura-json.js
@@ -2,3 +2,14 @@ const estudante = require('./estudante.json');
 
 console.log(estudante);
 console.log(typeof estudante);
+
+const stringEstudante = JSON.stringify(estudante);
+console.log(stringEstudante);
+console.log(typeof stringEstudante);
+
+console.log(estudante.nome);
+console.log(stringEstudante.nome);
+
+const objetoEstudante = JSON.parse(stringEstudante);
+console.log(typeof objetoEstudante);
+console.log(objetoEstudante.nome);
