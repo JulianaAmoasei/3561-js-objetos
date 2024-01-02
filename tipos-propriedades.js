0a491b4300fb8087a01bcc827b080ca37dfdd58a
JulianaAmoasei <late.parrot@gmail.com>
2024-01-02 15:16:39 -0300

    video: tipos de dados e valores

---
+++ tipos-propriedades.js
@@ -0,0 +1,11 @@
+const estudante = {
+  nome: 'Ana',
+  idade: 32,
+  cpf: '23423423423',
+  email: 'ana@email.com',
+  telefones: ['551198745632', '551198745633'],
+};
+
+console.log(estudante);
+console.log(estudante.telefones);
+console.log(estudante.telefones[0]);
